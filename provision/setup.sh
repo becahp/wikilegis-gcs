2d0a8be7116159b0da0e4e19fb4275a5d7f578d4
caiquepereira <caiquepereira@gmail.com>
2016-06-02 15:03:51 -0300

    Fixed problem with libsass and Pillow install

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,5 +1,12 @@
 sudo apt-get -y update
 sudo apt-get -y install git bash
 sudo apt-get install -y python-pip
-cd /vagrant/wikilegis/
-sudo pip install -r ../requirements.txt
+sudo apt-get install -y gcc
+sudo apt-get install -y g++
+sudo apt-get install -y python-dev
+sudo pip install libsass
+sudo apt-get build-dep python-imaging
+sudo apt-get install -y libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
+sudo pip install Pillow
+cd /vagrant/
+sudo pip install -r requirements.txt
