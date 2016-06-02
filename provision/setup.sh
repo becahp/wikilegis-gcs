a8b8a74663f81bcd635dccd6b2284f96da4071aa
caiquepereira <caiquepereira@gmail.com>
2016-06-02 15:34:00 -0300

    Removed unnecessary installations on setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,11 +1,9 @@
 sudo apt-get -y update
 sudo apt-get -y install git bash
 sudo apt-get install -y python-pip
-sudo apt-get install -y gcc
 sudo apt-get install -y g++
 sudo apt-get install -y python-dev
 sudo pip install libsass
-sudo apt-get build-dep python-imaging
 sudo apt-get install -y libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
 sudo pip install Pillow
 cd /vagrant/
